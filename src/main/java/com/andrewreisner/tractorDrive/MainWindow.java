8761232d1533f99b0b3a28bdfbf62a78534d26fe
Andrew Reisner <andrew@reisner.co>
2013-04-10 17:08:08 -0500

    basic mainwindow format

--- src/main/java/com/andrewreisner/tractorDrive/MainWindow.java
+++ src/main/java/com/andrewreisner/tractorDrive/MainWindow.java
@@ -1,10 +1,21 @@
+package com.andrewreisner.tractorDrive;
 import java.awt.EventQueue;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.UIManager;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JSplitPane;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
 
 public class MainWindow {
 
@@ -52,10 +63,54 @@ public class MainWindow {
 		JFileChooser fchoose = new JFileChooser();
 		fchoose.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		int retval = fchoose.showOpenDialog(frame.getContentPane());
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.SOUTH);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblStatus = new JLabel("Ready");
+		panel.add(lblStatus, BorderLayout.WEST);
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setEnabled(false);
+		panel.add(splitPane, BorderLayout.EAST);
+		
+		JButton btnUpdate = new JButton("update");
+		splitPane.setLeftComponent(btnUpdate);
+		
+		JButton btnCancel = new JButton("cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				System.exit(1);
+			}
+		});
+		splitPane.setRightComponent(btnCancel);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setContentType("text/html");
+		textPane.setEditable(false);
+		
+		JScrollPane jsp = new JScrollPane(textPane);
+		frame.getContentPane().add(jsp, BorderLayout.CENTER);
+		
 		if (retval == JFileChooser.APPROVE_OPTION) {
 			try {
-				TractorRepo tractorRepo = new TractorRepo(fchoose.getSelectedFile().getAbsolutePath());
+					TractorRepo tractorRepo = new TractorRepo(fchoose.getSelectedFile().getAbsolutePath());
+					tractorRepo.fetchUpdates(null);
+					StringBuilder html = new StringBuilder();
+					html.append("<html><body><h1>Current Release: " + tractorRepo.getVersion() + "</h1>");
+					String[] updates = tractorRepo.getUpdates();
+					html.append("<h2>Found " +  updates.length + " updates.</h2><hr />");
+					for (int i=updates.length-1;i>=0;i--) {
+						html.append("<pre>");
+						html.append(tractorRepo.getReleaseNotes(updates[i]));
+						html.append("</pre><hr />");
+					}
+					html.append("</body></html>");
+					textPane.setText(html.toString());
+					textPane.setCaretPosition(0);
 			}catch (Exception ex){
+				ex.printStackTrace();
 				JOptionPane.showMessageDialog(frame, "Invalid TractorDrive directory!");
 				System.exit(1);
 			}
